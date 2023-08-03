ee6d36a214e9a03011c7eb260fbb4f399d8637ab
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-03 14:04:06 +0500

    CODE RUB : Implement StorageBroker Methods

--- AuthenticationAndauthorization/Brokers/StorageBrokers/StorageBroker.Products.cs
+++ AuthenticationAndauthorization/Brokers/StorageBrokers/StorageBroker.Products.cs
@@ -6,27 +6,17 @@ namespace AuthenticationAndauthorization.Brokers.StorageBrokers
     public partial class StorageBroker
     {
         public DbSet<Product> Products { get; set; }
-        public ValueTask<Product> InsertProductAsync(Product product)
-        {
-            throw new NotImplementedException();
-        }
+        public async ValueTask<Product> InsertProductAsync(Product product) =>
+            await InsertAsync(product);
 
-        public IQueryable<Product> SelectAllProducts()
-        {
-            throw new NotImplementedException();
-        }
+        public IQueryable<Product> SelectAllProducts() =>
+            SelectAll<Product>();
 
-        public ValueTask<Product> SelectProductByIdAsync(Guid productId)
-        {
-            throw new NotImplementedException();
-        }
-        public ValueTask<Product> UpdateProductAsync(Product product)
-        {
-            throw new NotImplementedException();
-        }
-        public ValueTask<Product> DeleteProductAsync(Product product)
-        {
-            throw new NotImplementedException();
-        }
+        public async ValueTask<Product> SelectProductByIdAsync(Guid productId) =>
+            await SelectAsync<Product>(productId);
+        public async ValueTask<Product> UpdateProductAsync(Product product) =>
+            await UpdateAsync(product);
+        public async ValueTask<Product> DeleteProductAsync(Product product) =>
+            await DeleteAsync(product);
     }
 }
